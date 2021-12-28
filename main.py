cfd823cadaaa0cb0a0b94bc12f4840463b1ec4a9
Shreesh Pandey <shvspandey@gmail.com>
2021-12-28 23:00:52 +0530

    Establishing Connection with database

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-'''Displaying Welcome Message On the Master Branch
-    '''
+"""Here We are Practising the DataBase Execution
+    using pymysql"""
+import errno
 
-print("Welcome To The DataBase Execution Practise Problem")
+import mysql.connector
+from mysql.connector import errorcode
 
+
+class DataBase:
+    @staticmethod
+    def connection_database():
+        try:
+            conn = mysql.connector.connect(host='losthost', user='root', port='2003', password='',
+                                           database='28december2021')
+            cursor = conn.cursor()
+        except mysql.connector.Error as er:
+            if er.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something Wrong with Username and password")
+            elif er.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database doe not exist")
+            else:
+                print(er)
+
+
+
+ss = DataBase.connection_database()
+print(ss)
