ef3671f43159389903ce4372bb54e4945dda661e
Shreesh Pandey <shvspandey@gmail.com>
2022-01-03 18:33:18 +0530

    Refractored:- The Retrieve Querie

--- databa.sql
+++ databa.sql
@@ -1,8 +1,12 @@
 create table details(first_name varchar(10),
 last_name varchar(10),
-mobile_no int(11),
+mobile_no varchar(11),
 email_id varchar(20),
 location varchar(20),
 state varchar(20)
 );
-
+show databases
+create database databa
+USE databa
+drop table details
+select * from details
